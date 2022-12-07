62770483a8363b7f8a2717594d1fb270f8178257
Ildar <ildar.azmatov@ya.ru>
2022-12-07 13:40:00 +0500

    поиск числа в массиве

---
+++ 33/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+Console.WriteLine("Задайте массив: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+bool count= false;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 5);
+    Console.Write($"{array[i]} ");
+    }
+Console.WriteLine("Звдайте число которовое вы ищите: ");
+int M =Convert.ToInt32(Console.ReadLine());
+for(int index=0;index<N; index++)
+{
+   if (array[index]==M)
+   {
+count= true;
+   }
+}
+Console.WriteLine(count);
