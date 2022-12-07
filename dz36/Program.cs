d43737719cbadf98ecf4bbcf32ee92d75c7f2e6a
Ildar <ildar.azmatov@ya.ru>
2022-12-07 14:55:56 +0500

    Сумма не четных чисел в массиве

---
+++ dz36/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Задайте массив: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.Write($"{array[i]} ");
+    if (i%2 ==0)
+    {
+        count += array[i];
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Сумма не четных чисел->" + count);
